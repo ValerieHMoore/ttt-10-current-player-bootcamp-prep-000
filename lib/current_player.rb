dd57c85e9353ec0d4fa5498fad5519f38dfd3358
Valerie Moore <periwinkle17@outlook.com>
2018-05-24 18:44:23 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -10,10 +10,9 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count % 2 == 0 || turn_count == 0
+  if turn_count(board) % 2 == 0 || turn_count(board) == 0
     puts "X"
   else
     puts "O"
   end
-current_player(board)
 end
